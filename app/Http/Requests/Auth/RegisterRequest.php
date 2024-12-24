12eff622a0c950f31ba9f768f51028773f2cb0a6
biletweb-macmini <bilet.igor@gmail.com>
2024-12-24 19:23:42 +0200

    добавлено фамилию при регистрации

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -23,6 +23,7 @@ class RegisterRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
+            'surname' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'max:255', 'confirmed'],
         ];
