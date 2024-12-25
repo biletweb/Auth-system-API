d61ac15d3d25abe9a982f615d9546f97423a7805
biletweb-macmini <bilet.igor@gmail.com>
2024-12-25 22:56:13 +0200

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,10 +13,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+        VerifyEmail::toMailUsing(function (object $notifiable) {
+            $securityCode = random_int(100000, 999999);
             return (new MailMessage)
                 ->subject('Verify Email Address')
-                ->view('emails.verify-email', ['name' => $notifiable->name, 'url' => $url]);
+                ->view('emails.verify-email', ['name' => $notifiable->name, 'securityCode' => $securityCode,]);
         });
     }
 
--- resources/views/emails/verify-email.blade.php
+++ resources/views/emails/verify-email.blade.php
@@ -1,5 +1,4 @@
 <div>
     <p>Hi {{ $name }},</p>
-    <p>Thanks for registering with us. Please click the link below to verify your email address.</p>
-    <a href="{{ $url }}">Verify Email</a>
+    <p>Thanks for registering with us. Verification code: {{ $securityCode }}</p>
 </div>
