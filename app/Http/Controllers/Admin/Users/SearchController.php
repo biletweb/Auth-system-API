f8b74cc3a59d653d1cb28d152a27b7b12e656930
biletweb-macmini <bilet.igor@gmail.com>
2024-12-28 15:09:58 +0200

    fix

--- app/Http/Controllers/Admin/Users/SearchController.php
+++ app/Http/Controllers/Admin/Users/SearchController.php
@@ -35,7 +35,8 @@ class SearchController extends Controller
                 // Если одно слово, ищем во всех полях
                 $query->where('name', 'like', '%'.$searchTerm.'%')
                     ->orWhere('surname', 'like', '%'.$searchTerm.'%')
-                    ->orWhere('email', 'like', '%'.$searchTerm.'%');
+                    ->orWhere('email', 'like', '%'.$searchTerm.'%')
+                    ->orWhere('role', 'like', '%'.$searchTerm.'%');
             })
             ->orderByDesc('id')
             ->get();
